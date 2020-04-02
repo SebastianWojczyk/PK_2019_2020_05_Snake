4e0ab4c41439dc2896877f05e3d5090aefb72b71
Sebastian Wojczyk <sebastian.wojczyk@wmii.uni.lodz.pl>
2020-04-02 10:51:43 +0200

    Pokazowy commit: 1. Jak działa timer 2. Jak zdefiniować i obsłużyć własny event.

--- 05_Snake/Form1.cs
+++ 05_Snake/Form1.cs
@@ -12,9 +12,17 @@ namespace _05_Snake
 {
     public partial class Form1 : Form
     {
+        SnakeLogic mySnakeLogic;
         public Form1()
         {
             InitializeComponent();
+            mySnakeLogic = new SnakeLogic();
+            mySnakeLogic.SnakeMoved += MySnakeLogic_SnakeMoved;
+        }
+
+        private void MySnakeLogic_SnakeMoved()
+        {
+            this.Text += " #";
         }
     }
 }
---
+++ 05_Snake/SnakeLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_Snake
+{
+
+    class SnakeLogic
+    {
+        public delegate void SnakeEvent();
+        public event SnakeEvent SnakeMoved;
+
+        Timer timer = new Timer();
+
+        public SnakeLogic()
+        {
+            timer.Enabled = true;
+            timer.Interval = 200;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (SnakeMoved != null)
+            {
+                SnakeMoved();
+            }
+        }
+    }
+}
